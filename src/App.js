3e9acb924caa7e5c8f95d66ea7b0811e84a46fab
AosafHimu <92543405+AosafHimu@users.noreply.github.com>
2023-06-19 21:26:14 +1000

    quick hack

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/Md-Aosaful-Alam" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/project/:id" element={<ProjectDisplay />} />
           <Route path="/experience" element={<Experience />} />
